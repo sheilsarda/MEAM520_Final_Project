0dcd90901128ba9863b5944e21dbdb81119ec9ba
sheil sarda <sheilsarda@yahoo.com>
2020-11-29 15:13:02 -0500

    visualizing obstacles

--- final_test.py
+++ final_test.py
@@ -19,23 +19,26 @@ from random import random as rand
 fig = plt.figure(figsize=(20, 20))
 ax = fig.add_subplot(111, projection='3d')
 
+ax.set_xlim3d([-600, 600])
+ax.set_ylim3d([-600, 600])
+ax.set_zlim3d([-600, 600])
+
 map1 = loadmap("maps/final.txt")
 obs = map1.obstacles
 
 start = np.array([0,  0, 0, 0, 0, 0])
 goal  = np.array([0, 0, 0, 0, 0, 0])
 
-path = Astar(deepcopy(map1), deepcopy(start), deepcopy(goal))
+# path = Astar(deepcopy(map1), deepcopy(start), deepcopy(goal))
     
-print("Astart path : ")
-print(path)
+# print("Astart path : ")
+# print(path)
 
-f = calculateFK()
-p_start = f.forward(path[0])[0]
-line = ax.plot(p_start[:, 0], p_start[:, 1], p_start[:, 2])[0]
+# f = calculateFK()
+# p_start = f.forward(path[0])[0]
+# line = ax.plot(p_start[:, 0], p_start[:, 1], p_start[:, 2])[0]
 
 visualize.plot_obstacles(ax, obs)
-arm = animation.FuncAnimation(fig, visualize.animate, fargs=(path, line),
-                                  interval=20, blit=False)
+# arm = animation.FuncAnimation(fig, visualize.animate, fargs=(path, line), interval=20, blit=False)
 
 plt.show()
