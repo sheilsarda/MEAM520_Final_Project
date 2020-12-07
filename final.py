e5c002c2397459157145aabf52be07c77f3421bd
sheilsarda <sheilsarda@yahoo.com>
2020-12-07 15:29:09 -0500

    pickup all dynamic blocks

--- final.py
+++ final.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python2
-
-import rospy
-
+from arm_controller import ArmController
 from time import sleep
 import numpy as np
+import rospy
 import sys
 from random import random as rand
 from gripStatic import calcNewQ4
 from calcIK import inverse
 from IK_velocity import IK_velocity
+import rospy
 import sys
 from os import getcwd
 from copy import deepcopy
-
 sys.path.append(getcwd() + "/../Core")
-from arm_controller import ArmController
+
 
 # Function definition to set paths using command
+
 def setpath(finq):
     ispos = False
     lynx.command(finq)
+    print("setting path")
     while ispos == False:
-        sleep(3)
+        sleep(1)
         curq, _ = lynx.get_state()
-        print(curq)
+        # print(curq)
         if abs(curq[0]-finq[0]) < 0.05:
             if abs(curq[1]-finq[1]) < 0.05:
                 if abs(curq[2]-finq[2]) < 0.05:
@@ -33,19 +34,43 @@ def setpath(finq):
 
 
 # Finding the closest block to track within the dynamic blocks
-def closestblock():
+def closestblock(color):
     [name, pose, twist] = lynx.get_object_state()
-    block_ang = np.empty(len(dynamicBlocks))
-    for r in range(0, len(dynamicBlocks)):
-        block_ang[r] = (3*np.pi/2-0.1)-np.arctan2(pose[dynamicBlocks[r]]
-                                                  [1, -1], pose[dynamicBlocks[r]][0, -1])
-        if block_ang[r] < 0:
-            block_ang[r] = block_ang[r]+1000
-    print("block angles:", block_ang)
-    i = np.argmin(block_ang)
-    # return index of dynamicBlocks to pickup
-    print("index:", i)
-    return i
+    avail_block = avail_dyn()
+    block_ang = np.empty(len(avail_block))
+    ang_diff = np.empty(len(avail_block))
+    for r in range(0, len(avail_block)):
+        n = dynamicBlocks.index(avail_block[r])
+        if np.arctan2(pose[dynamicBlocks[n]][1, -1], pose[dynamicBlocks[n]][0, -1]) < 0:
+            block_ang[r] = np.arctan2(
+                pose[dynamicBlocks[n]][1, -1], pose[dynamicBlocks[n]][0, -1])+2*np.pi
+        else:
+            block_ang[r] = np.arctan2(
+                pose[dynamicBlocks[n]][1, -1], pose[dynamicBlocks[n]][0, -1])
+        if color == "red":
+            ang_diff[r] = ((5*np.pi)/4-0.05)-block_ang[r]
+        else:
+            ang_diff[r] = ((np.pi)/4-0.05)-block_ang[r]
+    print("available blocks", ang_diff)
+    i = np.argmin([b for b in ang_diff if b >= 0])
+    print("chosen", ang_diff[i])
+    f = dynamicBlocks.index(avail_block[i])
+    print("confirmation", pose[dynamicBlocks[f]])
+    return f
+
+
+def avail_dyn():
+    _, pose, _ = lynx.get_object_state()
+    # list of dynamic blocks on platform
+    dyn_platform = []
+    for n in range(len(dynamicBlocks)):
+        if pose[dynamicBlocks[n]][0, -1] <= 100 and pose[dynamicBlocks[n]][0, -1] >= -100:
+            if pose[dynamicBlocks[n]][1, -1] <= 100 and pose[dynamicBlocks[n]][1, -1] >= -100:
+                if pose[dynamicBlocks[n]][2, -1] <= 80 and pose[dynamicBlocks[n]][2, -1] >= 40:
+                    dyn_platform.append(dynamicBlocks[n])
+    print("blocks available", dyn_platform)
+    return dyn_platform
+
 
 if __name__ == '__main__':
 
@@ -56,7 +81,7 @@ if __name__ == '__main__':
     color = sys.argv[1]
     lynx = ArmController(color)
 
-    sleep(10)  # wait for setup
+    sleep(1)  # wait for setup
 
     # lynx.wait_for_start() # Wait for Start Gun to be fired
 
@@ -72,13 +97,13 @@ if __name__ == '__main__':
     print("Object twist:", twist)
 
     # get state of your opponent's robot
-    # [q, qd]  = lynx.get_opponent_state()
-    # print("Opponent position:",q)
-    # print("Opponent velocity:",qd)
+    [q, qd] = lynx.get_opponent_state()
+    print("Opponent position:", q)
+    print("Opponent velocity:", qd)
 
-    # Dynamic block test
-    # setpath([0.9,0.05,np.pi/5,0,1.5,30])
-    # Separate based on dynamic vs static (red v blue)
+    # Dynamic block test - separate based
+    # on dynamic vs static (red v blue)
+
     numBlocks = len(name)
     dynamicBlocks = []
     staticBlocks = []  # Maybe sort indices by distance to goal
@@ -92,7 +117,7 @@ if __name__ == '__main__':
             # Choose statics by which side of the world they are on (Y position)
             if pose[n][1, 3] < 0 and color == "red":
                 staticBlocks.append(n)
-            elif color == "blue":
+            elif pose[n][1, 3] > 0 and color == "blue":
                 staticBlocks.append(n)
 
     print("Dynamic Blocks: " + str(dynamicBlocks))
@@ -114,65 +139,59 @@ if __name__ == '__main__':
     print("goalPlatform:", goalPlatform)
 
     # Dynamic test:
-    # STARTING POSE FOR DYNAMIC
-    Tf = np.array([[-0.5, 0, 0.5, 110], [0.5, 0, 0.5, 110],
-                   [0, 1, 0, 85], [0, 0, 0, 1]])
-    newq = inverse(Tf, 30)
-    setpath(newq)
-    # [name, pose, twist] = lynx.get_object_state()
-    
-    i = closestblock()
-    print("index:", i)
-    chosen_block = dynamicBlocks[i]
-    print("Twist:", twist[chosen_block])
-    print("Pose:", pose[chosen_block])
+    while len(avail_dyn()) > 0:
+        Tf = np.array([[-0.5, 0, 0.5, 110], [0.5, 0, 0.5, 110],
+                       [0, 1, 0, 75], [0, 0, 0, 1]])
+        newq = inverse(Tf, 30)
+        setpath(newq)
+        # [name, pose, twist] = lynx.get_object_state()
+        i = closestblock(color)
+        print("index:", i)
+        chosen_block = dynamicBlocks[i]
+        rost = rospy.get_time()
+        A = np.sqrt(2*(200**2))
+        block_r = np.sqrt(pose[chosen_block][0, -1] **
+                          2+pose[chosen_block][1, -1]**2)
+        # Setting position within 30 mm to blocks
+        block_offset = (A-(block_r+50))/np.sqrt(2)
+        Tf_bl = deepcopy(Tf)
+        Tf_bl[0, -1] = block_offset
+        Tf_bl[1, -1] = block_offset
+        newq = inverse(Tf_bl, 30)
+        setpath(newq)
+        t0e = pose[chosen_block]
+        btheta = np.arctan2(t0e[1, -1], t0e[0, -1])
+        # Setting position to scoop up and go 10 mm ahead
+        block_offset = (A-(block_r-5))/np.sqrt(2)
+        Tf_bl1 = deepcopy(Tf)
+        Tf_bl1[0, -1] = block_offset
+        Tf_bl1[1, -1] = block_offset
+        Tf_bl1[2, -1] = 70
+        newq = inverse(Tf_bl1, 30)
+        cur_block_ang = np.arctan2(
+            pose[chosen_block][1, -1], pose[chosen_block][0, -1])
+        if cur_block_ang < 0:
+            cur_block_ang += np.pi
+        while cur_block_ang <= ((5*np.pi)/4)-0.05:
+            _, pose, _ = lynx.get_object_state()
+            # print(np.arctan2(pose[chosen_block][1,-1],pose[chosen_block][0,-1]))
+            cur_block_ang = np.arctan2(
+                pose[chosen_block][1, -1], pose[chosen_block][0, -1])
+            if cur_block_ang < 0:
+                cur_block_ang += 2*np.pi
+            print("waiting for block")
+        setpath(newq)
+        newq = inverse(Tf_bl1, 0)
+        setpath(newq)
+        Tf_bl2 = deepcopy(Tf_bl1)
+        Tf_bl2[2, -1] = 110
+        newq = inverse(Tf_bl2, 0)
+        setpath(newq)
+        setpath([0, 0, 0, 0, 0, 0])
+        setpath([-1.3, 1.2, -1.5, np.pi/2, -np.pi/2, 0])
+        setpath([-1.3, 1.2, -1.5, np.pi/2, -np.pi/2, 30])
+        avail_blocks = avail_dyn()
 
-    rost = rospy.get_time()
-    
-    angv = twist[chosen_block][-1]
-    block_r = np.sqrt(pose[chosen_block][0, -1]**2 +
-                      pose[chosen_block][1, -1]**2)
-    
-    # Setting position within 30 mm to blocks
-    hyp = 200*np.sqrt(2)-(block_r+30)
-    block_offset = np.sin(np.pi/4)*hyp
-    print("block offset:", block_offset)
-    print("block+30:", block_r+30)
-    # block_offset=200-(block_r+30)/np.sqrt(2)
-    
-    Tf_bl = deepcopy(Tf)
-    Tf_bl[0, -1] = block_offset
-    Tf_bl[1, -1] = block_offset
-    newq = inverse(Tf_bl, 30)
-    setpath(newq)
-
-    t0e = pose[chosen_block]
-    btheta = np.arctan2(t0e[1, -1], t0e[0, -1])
-    setpost = ((3*np.pi/2)-btheta)/angv
-    # print(setpost+rost)
-    
-    dq = IK_velocity(newq, np.array([50, 50, 0]), np.array([0, 0, 0]), 6)
-    dq[-1] = 30
-    
-    while rospy.get_time() < setpost+rost-5:
-        print("waiting for block")
-        pass
-    
-    # bset = rospy.get_time()
-    # while rospy.get_time()<bset+1:
-    #     lynx.set_vel(dq)
-    #     curq,_= lynx.get_state()
-    #     dq=IK_velocity(curq,np.array([50,50,0]),np.array([0,0,0]),6)
-    #     print("getting block")
-
-    curq, _ = lynx.get_state()
-    curq[-1] = 0
-    lynx.set_pos(curq)
-    sleep(2)
-    setpath([-1.4, 0.3, -0.3, 1.2456, -2, 0])
-
-    # zeroPose = [0, 0, 0, 0, 0, 0]
-    # setpath(zeroPose)
 
     for i in range(0, len(platform2)):
 
