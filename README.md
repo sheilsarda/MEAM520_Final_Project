a53aafc33e8212c0dc91cb7c631d6a15abc697b7
sheilsarda <sheilsarda@yahoo.com>
2021-03-10 14:11:45 -0500

    added readme

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # MEAM520 Final Project
 
-### Venkata Gurrala
+Fall 2020, Taught by Prof. Cynthia Sung
 
-### Sheil Sarda
+Brian Grimaldi, Venkata Gurrala, Sheil Sarda, Alexis Ward
 
-### Brian Grimaldi
+### Overview
 
-### Alexis Ward
+Teams will use the concepts learned during the semester to control their simulated Lynx robot in a
+head-to-head competition with their opponents’ robot. The robots will manipulate objects in the simulated
+environment to score points, culminating in a class-wide tournament.
 
-## Files from the Repository submitted
+[**Final Report detailing our strategy and implementation**](/Report/FinalProject.pdf)
+
+<img src="imgs/Virtual_env.png" width=800>
+
+### Files required to run our project in ROS + Gazebo
 
 ```sh
 ├── IK_velocity.py
--- final.py
+++ final.py
@@ -38,44 +38,27 @@ def setpath(finq):
 def closestblock(color):
     [name, pose, twist] = lynx.get_object_state()
     avail_block=avail_dyn()
-
-    if(len(avail_block) == 0): return -1
-
     block_ang=np.empty(len(avail_block))
     ang_diff=np.empty(len(avail_block))
     for r in range(0,len(avail_block)):
         n=dynamicBlocks.index(avail_block[r])
-        # if np.arctan2(pose[dynamicBlocks[n]][1,-1],pose[dynamicBlocks[n]][0,-1])<0:
-        #     block_ang[r]=np.arctan2(pose[dynamicBlocks[n]][1,-1],pose[dynamicBlocks[n]][0,-1])+2*np.pi
-        # else:
-        #     block_ang[r]=np.arctan2(pose[dynamicBlocks[n]][1,-1],pose[dynamicBlocks[n]][0,-1])
+        if np.arctan2(pose[dynamicBlocks[n]][1,-1],pose[dynamicBlocks[n]][0,-1])<0:
+            block_ang[r]=np.arctan2(pose[dynamicBlocks[n]][1,-1],pose[dynamicBlocks[n]][0,-1])+2*np.pi
+        else:
+            block_ang[r]=np.arctan2(pose[dynamicBlocks[n]][1,-1],pose[dynamicBlocks[n]][0,-1])
         if color == "red":
-            if np.arctan2(pose[dynamicBlocks[n]][1,-1],pose[dynamicBlocks[n]][0,-1])<0:
-                block_ang[r]=np.arctan2(pose[dynamicBlocks[n]][1,-1],pose[dynamicBlocks[n]][0,-1])+2*np.pi
-            else:
-                block_ang[r]=np.arctan2(pose[dynamicBlocks[n]][1,-1],pose[dynamicBlocks[n]][0,-1])
             ang_diff[r]=((5*np.pi)/4-0.05)-block_ang[r]
             pos_vals=np.array([b for b in ang_diff if b>=0])
-        else:
-            # if block_ang[r]>np.pi/4:
-            #     ang_diff[r]=(2*np.pi)-block_ang[r]+(np.pi/4)
-            # else:
-            #     ang_diff[r]=(np.pi/4)-block_ang[r]
-            block_ang[r]=np.arctan2(pose[dynamicBlocks[n]][1,-1],pose[dynamicBlocks[n]][0,-1])
-            if block_ang[r]<0:
-                ang_diff[r]=-block_ang[r]+(np.pi/4)
-            elif block_ang[r]>np.pi/4:
-                ang_diff[r]=(np.pi-block_ang[r])+np.pi+np.pi/4
+            if len(pos_vals)>0:
+                i=np.argmin([b for b in ang_diff if b>=0])
             else:
-                 ang_diff[r]=np.pi/4-block_ang[r]
-    if color == "red":
-        i=np.argmin(ang_diff)
-        if len(pos_vals)>0:
-            i=np.argmin([b for b in ang_diff if b>=0])
+                i=np.argmax(ang_diff)
         else:
-            i=np.argmax(ang_diff)
-    else:
-        i=np.argmin(ang_diff)
+            if block_ang[r]>np.pi/4:
+                ang_diff[r]=(2*np.pi)-block_ang[r]+(np.pi/4)
+            else:
+                ang_diff[r]=(np.pi/4)-block_ang[r]
+            i=np.argmin(ang_diff)
     print("available blocks",ang_diff)
     # i=np.argmin([b for b in ang_diff if b>=0])
     print("chosen",ang_diff[i])
@@ -95,25 +78,7 @@ def avail_dyn():
     print("blocks available", dyn_platform)
     return dyn_platform
 
-def plat_blocks(staticBlocks):
-    name,pose,_= lynx.get_object_state()
-    platform1 = []
-    platform2 = []
-    goalPlatform=[]
-    for r in range(0, len(staticBlocks)):
-        if pose[staticBlocks[r]][2, -1] < 10 and pose[staticBlocks[r]][2, -1] > 5:
-            platform1.append(staticBlocks[r])
-        elif pose[staticBlocks[r]][2, -1] < 30 and pose[staticBlocks[r]][2, -1] > 5:
-            platform2.append(staticBlocks[r])
-        elif pose[staticBlocks[r]][2, -1]>40 and abs(pose[staticBlocks[r]][1, -1])>200:
-            goalPlatform.append(staticBlocks[r])
 
-    print("platform1   :", platform1)
-    print("platform2   :", platform2)
-    print("goalPlatform:", goalPlatform)
-    return platform1, platform2, goalPlatform
-
-    
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
@@ -164,14 +129,29 @@ if __name__ == '__main__':
 
     print("Dynamic Blocks: " + str(dynamicBlocks))
     print("Static Blocks:  " + str(staticBlocks))
-    platform1,platform2,goalPlatform_stat=plat_blocks(staticBlocks)
 
-    # tf test:
-    if color == "red":
-        goalPos = np.array([-110.0, -510.0, 50.5])
+    platform1 = []
+    platform2 = []
+    goalPlatform = []
+    for r in range(0, len(staticBlocks)):
+        if pose[staticBlocks[r]][2, -1] < 10 and pose[staticBlocks[r]][2, -1] > 5:
+            platform1.append(staticBlocks[r])
+        elif pose[staticBlocks[r]][2, -1] < 30 and pose[staticBlocks[r]][2, -1] > 5:
+            platform2.append(staticBlocks[r])
+        else:
+            goalPlatform.append(staticBlocks[r])
+
+    print("platform1   :", platform1)
+    print("platform2   :", platform2)
+    print("goalPlatform:", goalPlatform)
+
+    #tf test:
+    if color=="red":
+        Tf_goal, _ =horizontalAngle([-110,-510,120], color)
+        q_drop=inverse(Tf_goal,-15)
     else:
-        goalPos = np.array([110.0, 510.0, 50.5])
-    currStackDyn = 0
+        Tf_goal, _ =horizontalAngle([100,450,180], color) #110,510,120
+        q_drop=inverse(Tf_goal,-15)
     # setpath(newq)
     #Making an array to
     #
@@ -195,10 +175,8 @@ if __name__ == '__main__':
         newq=inverse(Tf_bl,30)
         setpath(newq)
         t0e=pose[chosen_block]
-        
-      
         # btheta=np.arctan2(t0e[1,-1],t0e[0,-1])
-        # Setting position to scoop up and go 10 mm ahead
+        #Setting position to scoop up and go 10 mm ahead
         block_offset=(A-(block_r-5))/np.sqrt(2)
         Tf_bl1=deepcopy(Tf)
         Tf_bl1[0,-1]=block_offset
@@ -212,41 +190,34 @@ if __name__ == '__main__':
                 cur_block_ang+=np.pi
             while cur_block_ang<=((5*np.pi)/4)-0.05:
                 _,pose,_=lynx.get_object_state()
-                if (abs(pose[dynamicBlocks[i]][1,3])  < 100):
-                    # print(np.arctan2(pose[chosen_block][1,-1],pose[chosen_block][0,-1]))
-                    cur_block_ang=np.arctan2(pose[chosen_block][1,-1],pose[chosen_block][0,-1])
-                    if cur_block_ang<0:
-                        cur_block_ang+=2*np.pi
-                    print("waiting for block")
-                    print("red")
+                # print(np.arctan2(pose[chosen_block][1,-1],pose[chosen_block][0,-1]))
+                cur_block_ang=np.arctan2(pose[chosen_block][1,-1],pose[chosen_block][0,-1])
+                if cur_block_ang<0:
+                    cur_block_ang+=2*np.pi
+                print("waiting for block")
+                print("red")
         else:
-            # if cur_block_ang<0:
-            #     ang_dif=-np.pi-cur_block_ang+(np.pi/4)
-            # elif cur_block_ang>(np.pi)/4:
-            #     ang_dif=(2*np.pi)-cur_block_ang+(np.pi/4)
-            # else:
-            #     ang_dif=(np.pi/4)-cur_block_ang
             if cur_block_ang<0:
-                ang_dif=-cur_block_ang+(np.pi/4)
+                ang_dif=-np.pi-cur_block_ang+(np.pi/4)
             elif cur_block_ang>(np.pi)/4:
-                ang_dif=(np.pi-cur_block_ang)+(np.pi)+(np.pi/4)
+                ang_dif=(2*np.pi)-cur_block_ang+(np.pi/4)
             else:
                 ang_dif=(np.pi/4)-cur_block_ang
             # print("current block angle:",cur_block_ang)
             while ang_dif>0.04:
                 _,pose,_=lynx.get_object_state()
-                    # print("block position:",np.arctan2(pose[chosen_block][1,-1],pose[chosen_block][0,-1]))
-                if (abs(pose[dynamicBlocks[i]][1,3])  < 100):
-                    cur_block_ang=np.arctan2(pose[chosen_block][1,-1],pose[chosen_block][0,-1])
-                    if cur_block_ang<0:
-                        ang_dif=-cur_block_ang+(np.pi/4)
-                    elif cur_block_ang>(np.pi)/4:
-                        ang_dif=(np.pi-cur_block_ang)+(np.pi)+(np.pi/4)
-                    else:
-                        ang_dif=(np.pi/4)-cur_block_ang
-                    print("current block angle:",cur_block_ang)
-                    print("current angle diff:",ang_dif)
-                    print("waiting for block")
+                # print("block position:",np.arctan2(pose[chosen_block][1,-1],pose[chosen_block][0,-1]))
+                cur_block_ang=np.arctan2(pose[chosen_block][1,-1],pose[chosen_block][0,-1])
+                if cur_block_ang<0:
+                    # cur_block_ang=-np.pi-cur_block_ang
+                    ang_dif=-np.pi-cur_block_ang+(np.pi/4)
+                elif cur_block_ang>(np.pi)/4:
+                    ang_dif=(2*np.pi)-cur_block_ang+(np.pi/4)
+                else:
+                    ang_dif=(np.pi/4)-cur_block_ang
+                print("current block angle:",cur_block_ang)
+                print("current angle diff:",ang_dif)
+                print("waiting for block")
         setpath(newq)
         newq=inverse(Tf_bl1,-15)
         setpath(newq)
@@ -255,49 +226,31 @@ if __name__ == '__main__':
         newq=inverse(Tf_bl2,-15)
         setpath(newq)
         setpath([0,0,0,0,0,-15])
-        # # setpath([-1.3,0,-1,0,0,0])
-        # # q_reach=deepcopy(q_drop)
-        # # q_release[]=
-        # setpath(q_drop)
-        # q_release=deepcopy(q_drop)
-        # q_release[-1]=30
-        # setpath(q_release)
-
-        # Going towards goal position
-        TgA, TgB = horizontalAngle(np.array([goalPos[0], goalPos[1], goalPos[2] + currStackDyn * 20.0]), color, [0.0, 120.0], angled=True)
-        qUp = inverse(TgB, -15)
-        
-        setpath(qUp)
-        newq = inverse(TgA, 0)
-        setpath(newq)
-
-        # Releasing the block at the position
-        newq[-1] = 30.0
-        setpath(newq)
-        newq = inverse(TgB, 30)
-        setpath(newq)
-
-        # TODO Raise the bot in some way
-        newq[1] -= 0.2
-        setpath(newq)
-        currStackDyn += 1
-    
+        # setpath([-1.3,0,-1,0,0,0])
+        # q_reach=deepcopy(q_drop)
+        # q_release[]=
+        setpath(q_drop)
+        q_release=deepcopy(q_drop)
+        q_release[-1]=30
+        setpath(q_release)
         setpath([0,0,0,0,0,0])
-
-    platform1,platform2,goalPlatform_stat=plat_blocks(staticBlocks)
 
     if color == "red":
         goalPos = np.array([-75.0, -475.0, 50.5]) #[-110.0, -510.0, 50.5])
     else:
         goalPos = np.array([75.0, 475.0, 50.5]) # [100.0, 450.0, 50.5]
-    
-    prevGoalLen = len(goalPlatform_stat) 
+
     currStack = 0
 
-    #for i in range(0, len(platform2)):
-    while len(platform2) > 0:
-        # print("PLATFORM 2 - Block " + str(i))
-        TfA, TfB = calcNewQ4(q, pose[platform2.pop(0)], color, [10.0, 60.0])
+    if color=="red":
+        Tf_goal, _ =horizontalAngle([-110,-510,120], color)
+        q_drop=inverse(Tf_goal,-15)
+    else:
+        Tf_goal, _ =horizontalAngle([100,450,180], color)
+        q_drop=inverse(Tf_goal,-15)
+    for i in range(0, len(platform2)):
+        print("PLATFORM 2 - Block " + str(i))
+        TfA, TfB = calcNewQ4(q, pose[platform2[i]], color, [10.0, 60.0])
         print(TfB)
         print(TfA)
         
@@ -318,15 +271,9 @@ if __name__ == '__main__':
         setpath(newq)
 
         # Going towards goal position
-        TgA, TgB = horizontalAngle(np.array([goalPos[0], goalPos[1], goalPos[2] + currStack * 20.0]), color, [0.0, 120.0], angled=True)
-        qUp = inverse(TgB, 0)
-        tempq0 = qUp[0]
-        qUp[0] = newq[0]
-        qUp[1] -= 0.15
-        setpath(qUp)
-        qUp[0] = tempq0
-        qUp[1] += 0.15
-        setpath(qUp)
+        TgA, TgB = horizontalAngle(np.array([goalPos[0], goalPos[1], goalPos[2] + currStack * 20.0]), color, [0.0, 50.0])
+        newq = inverse(TgB, 0) # Should start this even further up tho
+        setpath(newq)
         newq = inverse(TgA, 0)
         setpath(newq)
 
@@ -340,20 +287,14 @@ if __name__ == '__main__':
         newq[1] -= 0.2
         setpath(newq)
 
+        currStack += 1
         q = newq
-        platform1,platform2,goalPlatform_stat = plat_blocks(staticBlocks)
-        if prevGoalLen > len(goalPlatform_stat):
-            prevGoalLen = len(goalPlatform_stat)
-            currStack += 1
     print("end of platform 2")
 
-    
-
     # Getting right to the platform 1
-    # for i in range(0, len(platform1)):
-    while len(platform1) > 0:
+    for i in range(0, len(platform1)):
         print("PLATFORM 1 - Block " + str(i))
-        TfA, TfB = calcNewQ4(q, pose[platform1.pop(0)], color, [10.0, 60.0])
+        TfA, TfB = calcNewQ4(q, pose[platform1[i]], color, [10.0, 60.0])
         print(TfB)
         print(TfA)
         
@@ -374,15 +315,9 @@ if __name__ == '__main__':
         setpath(newq)
 
         # Going towards goal position
-        TgA, TgB = horizontalAngle(np.array([goalPos[0], goalPos[1], goalPos[2] + currStack * 20.0]), color, [0.0, 120.0], angled=True)
-        qUp = inverse(TgB, 0)
-        tempq0 = qUp[0]
-        qUp[0] = newq[0]
-        qUp[1] -= 0.15
-        setpath(qUp)
-        qUp[0] = tempq0
-        qUp[1] += 0.15
-        setpath(qUp)
+        TgA, TgB = horizontalAngle(np.array([goalPos[0], goalPos[1], goalPos[2] + currStack * 20.0]), color, [0.0, 50.0])
+        newq = inverse(TgB, 0) # Should start this even further up tho
+        setpath(newq)
         newq = inverse(TgA, 0)
         setpath(newq)
 
@@ -395,12 +330,9 @@ if __name__ == '__main__':
         # TODO Raise the bot in some way
         newq[1] -= 0.2
         setpath(newq)
-        
+
+        currStack += 1
         q = newq
-        platform1,platform2,goalPlatform_stat = plat_blocks(staticBlocks)
-        if prevGoalLen > len(goalPlatform_stat):
-            prevGoalLen = len(goalPlatform_stat)
-            currStack += 1
     print("end of platform 1")
 
     lynx.stop()
